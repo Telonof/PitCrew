2753605261c0ec08b5c9ad0e5b06b4e29e4d69bd
Telonof <terniyol@protonmail.com>
2025-06-09 19:50:09 -0400

    [GUI] Open folder of packaged mod not file

--- PitCrew/GUI/RightClickTree.cs
+++ PitCrew/GUI/RightClickTree.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -76,7 +77,7 @@ public class RightClickTree
 
         System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
         {
-            FileName = IM.currentMod.ID + ".zip",
+            FileName = Path.GetDirectoryName(Environment.ProcessPath),
             UseShellExecute = true
         });
     }
