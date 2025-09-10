7c3f5980cecc0d8abd4f33eb4a38e168a73a81c3
Telonof <115968757+Telonof@users.noreply.github.com>
2025-09-10 16:49:08 -0400

    [Version] Release 1.33

--- PitCrewCommon/Constants.cs
+++ PitCrewCommon/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string VERSION = "v1.32";
+        public const string VERSION = "v1.33";
         public const string METADATA_FOLDER = "pitcrewmetadata";
         public const string OODLE_FILE = "oo2core_5_win64.dll";
         public const string SERVER_DATA_FILE = "server_archetypes.entities.bin";
